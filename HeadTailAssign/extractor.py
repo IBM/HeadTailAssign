5061e4727e3084ced8deb36cc214ccd4b20af102
brendaferrari <brendafferrari@gmail.com>
2023-11-06 20:28:16 -0300

    fix: update extraction method

--- HeadTailAssign/extractor.py
+++ HeadTailAssign/extractor.py
@@ -70,52 +70,13 @@ class Extractor:
             raise ValueError("There is no files with .log extension.")
 
         return
-    # def op_extractor(self, name_dir: str): 
-    #     '''Extracts only the data regarding Mulliken Atomic Overlap Populations from .out file. Saves .txt file. Returns None.
-         
-    #     Arguments:
-    #         name_dir(str) = Name of the output directory'''
-    #     try:
-    #         for root, dirs, files in os.walk(name_dir):
-    #             for file in files:
-    #                 if file.endswith(".log"):
-    #                     fileName = re.sub(".log", "", file)
-    #                     path = root + "\\" + file 
-    #                     path_output = root + "\\" + fileName + ".txt"
-
-    #                     #https://stackoverflow.com/questions/61172255/how-to-slice-data-from-a-text-file-given-the-desired-range-of-lines
-    #                     with open(path, encoding='utf8') as f:
-    #                         with open(path_output, "w") as out:
-    #                             print(f'Analyzing {file}...')
-    #                             out.write(f'{file}\n')
-    #                             out.write('          ----- MULLIKEN ATOMIC OVERLAP POPULATIONS -----\n')
-    #                             should_write=False
-    #                             for x, line in enumerate(f):
-    #                                 if line.find('          ----- MULLIKEN ATOMIC OVERLAP POPULATIONS -----')!=-1:
-    #                                     should_write=False
-    #                                 if should_write:
-    #                                     out.write(line)
-    #                                 if line.find('               ----- POPULATIONS IN EACH AO -----')!=-1:
-    #                                     should_write=True
-
-    #                         if os.stat(path_output).st_size >= 100:
-    #                             print(f'file {fileName}.txt was generated sucessfully.')
-    #                         else:
-    #                             f.close()
-    #                             print(f'WARNING: {fileName} could not be generated.')
-    #                             os.remove(path_output)
-                                                
-    #     except ValueError:
-    #         raise ValueError("There is no files with .log extension.")
-
-    #     return
-
+    
     def rx_extractor(self, name_dir):
         '''Extracts the Rx value organized by higher to lower value. Returns a dataframe.
         
         Arguments:
             name_dir(str) = Name of the output directory.'''
-        global results
+        #global results
         try:
             for root, dirs, files in os.walk(name_dir):
                 for file in files:
@@ -126,26 +87,13 @@ class Extractor:
 
                         if os.stat(path).st_size >= 100:
                             print(f'Analyzing {file}...')
-                            #https://stackoverflow.com/questions/33538660/how-do-i-compare-2-lines-in-a-string-in-python
-                            with open(path, encoding='utf8') as f:
-                                valence_list = []
-                                for x, line in enumerate(f):
-                                    if re.search(r'\W \d', line):
-                                        values = line[17:42]
-
-                                        if bool(re.search(r'[^XYZ]Z', values[5:7])) == True:
-                                            z_orbital = values
-                                            valence_list.append(z_orbital)
-
                             helper = Helper()
-                            if len(valence_list) <= 1:
-                                ("WARNING: There is no atoms with z orbital in this monomer. Files will be deleted.")
-                                shutil.rmtree(root)
-                                break
-                            else:
-                                valence_orbital_list = helper._get_valence_orbital(valence_list)
-                                valence_orbital_list_updated = helper._get_valence_orbital_last(valence_list, valence_orbital_list)
-                                results = helper._get_dataframe(valence_orbital_list_updated, path_output)
+                            n_orbitals = helper._get_n_orbitals(path)
+                            atoms = helper._get_atoms_list(path)
+                            n_id = helper._get_id(path)
+                            a_population = helper._get_a_population(path, n_orbitals, n_id)
+
+                            results = helper._get_dataframe(atoms, n_id, a_population, path_output)
 
         except ValueError:
             raise ValueError("There is no files with .txt extension.")
