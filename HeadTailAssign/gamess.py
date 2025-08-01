74bc3469706f08ff16c90aa9e655672256f495fe
Brenda Ferrari <66972292+brendaferrari@users.noreply.github.com>
2025-08-01 12:29:36 +0300

    chore: update gamess.py

--- HeadTailAssign/gamess.py
+++ HeadTailAssign/gamess.py
@@ -131,7 +131,8 @@ class GAMESS:
             for file in files:
                 path = root + '\\' + file # use / to run in linux
                 if file.endswith(".inp"):
-                    os.system('python ././run_gamess_job.py '+path+' 2')
+                    os.system('python ././run_gamess_job.py '+path+' 2') #change to run_gamess_job_linux.py
+                                                                            # to run on linux
 
         count = 0
         for file in glob.glob("*.log"):
